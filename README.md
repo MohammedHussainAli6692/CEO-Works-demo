af665a6e4161f2e712a37dfbda30a7e11513ce8c
Mohammed Hussain Ali <106290784+MohammedHussainAli6692@users.noreply.github.com>
2022-05-26 08:05:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CEO-Works-demo" 
---
+++ app.js
@@ -0,0 +1,16 @@
+// Navigation Tabs
+
+function companies()
+{
+    location.href="./index.html";
+}
+
+function projects()
+{
+    location.href="./projects.html";
+}
+
+function users()
+{
+    location.href="./users.html";
+}
